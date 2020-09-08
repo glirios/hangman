3b4af58e7e0405bb61448edc86f8db0e1450bef5
Giovanni Lirios <lirios311@gmail.com>
2020-09-08 14:04:10 -0700

    Adding changes and include api calling

--- hangman.py
+++ hangman.py
@@ -1,186 +1,162 @@
-#########################################################
-## File Name: hangman.py                               ##
-## Description: Starter for Hangman project - ICS3U    ##
-#########################################################
+########################################################################
+## File Name: hangman.py                                              ##
+## Description: Hangman game using words from Wordnik (eventually)    ##
+########################################################################
+
+from wordnik import swagger, WordsApi
+from secrets import API_KEY, API_URL
 import pygame
-import random
+import math
 
 pygame.init()
-winHeight = 480
-winWidth = 700
+winHeight = 500
+winWidth = 900
 win=pygame.display.set_mode((winWidth,winHeight))
-#---------------------------------------#
+
+client = swagger.ApiClient(API_KEY, API_URL)
+
 # initialize global variables/constants #
-#---------------------------------------#
-BLACK = (0,0, 0)
-WHITE = (255,255,255)
-RED = (255,0, 0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
-LIGHT_BLUE = (102,255,255)
+btn_font = pygame.font.SysFont("chalkduster", 20)
+guess_font = pygame.font.SysFont("chalkduster", 24)
+end_font = pygame.font.SysFont('sertomalankara', 40)
+hangman_font = pygame.font.SysFont("chalkduster", 50)
 
-btn_font = pygame.font.SysFont("arial", 20)
-guess_font = pygame.font.SysFont("monospace", 24)
-lost_font = pygame.font.SysFont('arial', 45)
-word = ''
-buttons = []
-guessed = []
-hangmanPics = [pygame.image.load('hangman0.png'), pygame.image.load('hangman1.png'), pygame.image.load('hangman2.png'), pygame.image.load('hangman3.png'), pygame.image.load('hangman4.png'), pygame.image.load('hangman5.png'), pygame.image.load('hangman6.png')]
+hangmanPics = [pygame.image.load('images/hangman%s.png' % i) for i in range(7)]
+background = pygame.image.load('images/background.png')
 
-limbs = 0
+# Redraws window #
+def redraw_game_window(word, buttons, guessed, limbs):
+	WHITE = (255, 255, 255)
+	win.blit(background, (0,0))
+	
+	# Draws buttons to select from
+	for btn in buttons:
+		if btn[4]:
+			pygame.draw.circle(win, WHITE, (btn[1], btn[2]), btn[3])
+			pygame.draw.circle(win, btn[0], (btn[1], btn[2]), btn[3] - 2)
+			letter = btn_font.render(chr(btn[5]), 1, WHITE)
+			win.blit(letter, (int(btn[1] - letter.get_width() / 2), int(btn[2] - letter.get_height() / 2)))
 
+	spaced = spacedOut(word, guessed)
+	label = guess_font.render(spaced, 1, WHITE)
+	length = label.get_width()
+	pic = hangmanPics[limbs]
+	hangman = hangman_font.render("HANGMAN", 1, WHITE)
 
-def redraw_game_window():
-    global guessed
-    global hangmanPics
-    global limbs
-    win.fill(GREEN)
-    # Buttons
-    for i in range(len(buttons)):
-        if buttons[i][4]:
-            pygame.draw.circle(win, BLACK, (buttons[i][1], buttons[i][2]), buttons[i][3])
-            pygame.draw.circle(win, buttons[i][0], (buttons[i][1], buttons[i][2]), buttons[i][3] - 2
-                               )
-            label = btn_font.render(chr(buttons[i][5]), 1, BLACK)
-            win.blit(label, (buttons[i][1] - (label.get_width() / 2), buttons[i][2] - (label.get_height() / 2)))
+	win.blit(hangman, (5, winHeight - (hangman.get_height() + 10)))
+	win.blit(label,(int(winWidth/2 - length/2), 400))
+	win.blit(pic, (int(winWidth/2 - pic.get_width()/2 + 20), 150))
+	pygame.display.update()
 
-    spaced = spacedOut(word, guessed)
-    label1 = guess_font.render(spaced, 1, BLACK)
-    rect = label1.get_rect()
-    length = rect[2]
-    
-    win.blit(label1,(winWidth/2 - length/2, 400))
-
-    pic = hangmanPics[limbs]
-    win.blit(pic, (winWidth/2 - pic.get_width()/2 + 20, 150))
-    pygame.display.update()
-
-
+# Pulls a random word from an api #
 def randomWord():
-    file = open('words.txt')
-    f = file.readlines()
-    i = random.randrange(0, len(f) - 1)
+	wordapi = WordsApi.WordsApi(client)
+	word = wordapi.getRandomWord()
 
-    return f[i][:-1]
+	return word.word
 
+# Determines what letters have already been identified #
+def spacedOut(word, guessed):
+	spacedWord = ''
+	for char in word:
+		if char.isalpha():
+			if char.upper() in guessed:
+				spacedWord += char + " " 
+			else:
+				spacedWord += '_ '
+		else:
+			spacedWord += char + ' '
+	return spacedWord
+			
+def buttonHit(x, y, buttons):
+	for btn in buttons:
+		if math.sqrt(math.pow(btn[1] - x, 2) + math.pow(btn[2] - y, 2)) <= btn[3]:
+			return btn[5]
+	return None
 
-def hang(guess):
-    global word
-    if guess.lower() not in word.lower():
-        return True
-    else:
-        return False
+def end(winner, word):
+	WHITE = (255, 255, 255)
+	pygame.time.delay(250)
+	win.blit(background, (0,0))
+	again = True
 
+	text = 'WINNER!, press any key to play again...' if winner else 'You lost, press any key to play again...'
+	
+	label = end_font.render(text, 1, WHITE)
+	wordTxt = end_font.render(word.upper(), 1, WHITE)
+	wordWas = end_font.render('The phrase was: ', 1, WHITE)
+	hangman = hangman_font.render("HANGMAN", 1, WHITE)
 
-def spacedOut(word, guessed=[]):
-    spacedWord = ''
-    guessedLetters = guessed
-    for x in range(len(word)):
-        if word[x] != ' ':
-            spacedWord += '_ '
-            for i in range(len(guessedLetters)):
-                if word[x].upper() == guessedLetters[i]:
-                    spacedWord = spacedWord[:-2]
-                    spacedWord += word[x].upper() + ' '
-        elif word[x] == ' ':
-            spacedWord += ' '
-    return spacedWord
-            
+	win.blit(hangman, (5, winHeight - (hangman.get_height() + 10)))
+	win.blit(label, (int(winWidth / 2 - label.get_width() / 2), 140))
+	win.blit(wordWas, (int(winWidth/2 - wordWas.get_width()/2), 245))
+	win.blit(wordTxt, (int(winWidth/2 - wordTxt.get_width()/2), 295))
+	
+	pygame.display.update()
 
-def buttonHit(x, y):
-    for i in range(len(buttons)):
-        if x < buttons[i][1] + 20 and x > buttons[i][1] - 20:
-            if y < buttons[i][2] + 20 and y > buttons[i][2] - 20:
-                return buttons[i][5]
-    return None
-
-
-def end(winner=False):
-    global limbs
-    lostTxt = 'You Lost, press any key to play again...'
-    winTxt = 'WINNER!, press any key to play again...'
-    redraw_game_window()
-    pygame.time.delay(1000)
-    win.fill(GREEN)
-
-    if winner == True:
-        label = lost_font.render(winTxt, 1, BLACK)
-    else:
-        label = lost_font.render(lostTxt, 1, BLACK)
-
-    wordTxt = lost_font.render(word.upper(), 1, BLACK)
-    wordWas = lost_font.render('The phrase was: ', 1, BLACK)
-
-    win.blit(wordTxt, (winWidth/2 - wordTxt.get_width()/2, 295))
-    win.blit(wordWas, (winWidth/2 - wordWas.get_width()/2, 245))
-    win.blit(label, (winWidth / 2 - label.get_width() / 2, 140))
-    pygame.display.update()
-    again = True
-    while again:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            if event.type == pygame.KEYDOWN:
-                again = False
-    reset()
+	while again:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+			if event.type == pygame.KEYDOWN:
+				again = False
 
 
-def reset():
-    global limbs
-    global guessed
-    global buttons
-    global word
-    for i in range(len(buttons)):
-        buttons[i][4] = True
+def reset(buttons):
+	for btn in buttons:
+		btn[4] = True
+	return (0, [], buttons, randomWord())
 
-    limbs = 0
-    guessed = []
-    word = randomWord()
+def main():
+	limbs = 0
+	guessed = []
+	buttons = []
+	word = ''
 
-#MAINLINE
+	# Setup buttons
+	increase = round(winWidth / 13)
+	for i in range(26):
+		if i < 13:
+			y = 40
+			x = 25 + (increase * i)
+		else:
+			x = 25 + (increase * (i - 13))
+			y = 85
+		buttons.append([(115,115,115), x, y, 20, True, 65 + i])
+		# buttons.append([color, x_pos, y_pos, radius, visible, char])
 
+	word = randomWord()
+	inPlay = True
+	print(word)
 
-# Setup buttons
-increase = round(winWidth / 13)
-for i in range(26):
-    if i < 13:
-        y = 40
-        x = 25 + (increase * i)
-    else:
-        x = 25 + (increase * (i - 13))
-        y = 85
-    buttons.append([LIGHT_BLUE, x, y, 20, True, 65 + i])
-    # buttons.append([color, x_pos, y_pos, radius, visible, char])
+	while inPlay:
+		redraw_game_window(word, buttons, guessed, limbs)
+		pygame.time.delay(10)
 
-word = randomWord()
-inPlay = True
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				inPlay = False
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					inPlay = False
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				clickPos = pygame.mouse.get_pos()
+				letter = buttonHit(clickPos[0], clickPos[1], buttons)
+				if letter != None:
+					guessed.append(chr(letter))
+					buttons[letter - 65][4] = False
+					if chr(letter).lower() not in word.lower():
+						if limbs != 5:
+							limbs += 1
+						else:
+							end(False, word)
+							(limbs, guessed, buttons, word) = reset(buttons)
+					else:
+						# print(spacedOut(word, guessed))
+						if spacedOut(word, guessed).count('_') == 0:
+							end(True, word)
+							(limbs, guessed, buttons, word) = reset(buttons)
+	pygame.quit()
 
-while inPlay:
-    redraw_game_window()
-    pygame.time.delay(10)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            inPlay = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                inPlay = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            clickPos = pygame.mouse.get_pos()
-            letter = buttonHit(clickPos[0], clickPos[1])
-            if letter != None:
-                guessed.append(chr(letter))
-                buttons[letter - 65][4] = False
-                if hang(chr(letter)):
-                    if limbs != 5:
-                        limbs += 1
-                    else:
-                        end()
-                else:
-                    print(spacedOut(word, guessed))
-                    if spacedOut(word, guessed).count('_') == 0:
-                        end(True)
-
-pygame.quit()
-
-# always quit pygame when done!
+if __name__ == "__main__":
+	main()
